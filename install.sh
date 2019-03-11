44fb9fcb022d317977facf5395bdc3bf2c09c906
Max Nachlinger <max.nachlinger@gmail.com>
2019-03-11 16:39:54 -0700

    Build work

--- install.sh
+++ install.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
-rm -rf ./include
-mkdir ./include
+tar -xvzf skia-include.tar.gz > /dev/null 2>&1
 
-# Checkout Skia and depot_tools in .build
-cd ./include
-git clone https://skia.googlesource.com/skia.git
-cd skia
-# a version I know works :)
-git checkout 29d5dec9a0783a033b921dc483fb98d565d684f6
-
-cd ../../
-
-# we're not building Skia here - just extract the archive
 if [[ "$OSTYPE" == "darwin"* ]]; then
   tar -xvzf mac.tar.gz > /dev/null 2>&1
 else
-  # TODO - additional linux checks and binaries
-  tar -xvzf ubuntu.tar.gz > /dev/null 2>&1
+  tar -xvzf linux.tar.gz > /dev/null 2>&1
 fi
