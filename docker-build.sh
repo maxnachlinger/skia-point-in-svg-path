baa23d2e14d59b6651759984db079e82e404d566
Max Nachlinger <max.nachlinger@gmail.com>
2019-03-11 17:19:49 -0700

    Build work

--- docker-build.sh
+++ docker-build.sh
@@ -3,27 +3,10 @@
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 
-rm -rf ./include
-
 docker build --tag=ubuntudev -f ./docker/Ubuntu .
 
 id=$(docker create ubuntudev)
 rm -rf include
 docker cp $id:/include ./include
 
-tar czf ubuntu.tar.gz ./include
-
 docker rm -v $id
-
-
-docker build --tag=centosdev -f ./docker/Centos .
-
-id=$(docker create centosdev)
-rm -rf include
-docker cp $id:/include ./include
-
-tar czf centos.tar.gz ./include
-
-docker rm -v $id
-
-# TODO - once centos build is done, create include dir and tar gz it
