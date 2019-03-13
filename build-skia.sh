e203830f3f86c0fc59d71d826210cca60d828998
Max Nachlinger <max.nachlinger@gmail.com>
2019-03-12 18:43:19 -0700

    Build work

--- build-skia.sh
+++ build-skia.sh
@@ -2,9 +2,9 @@
 
 # build Skia -Skia doesn't support CMAKE - so we're stuck with a bash script :(
 
-rm -rf ./include
-mkdir ./include
-cd ./include
+rm -rf ./.skia-build
+mkdir ./.skia-build
+cd ./.skia-build
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 git clone https://skia.googlesource.com/skia.git
@@ -19,9 +19,9 @@ git checkout 29d5dec9a0783a033b921dc483fb98d565d684f6
 python tools/git-sync-deps
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  gn gen out/Release --args="is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false extra_cflags_cc=[\"-frtti\"]"
+  gn gen out/Release --args="is_official_build=true is_debug=false skia_use_system_expat=false skia_use_system_icu=false skia_use_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false extra_cflags_cc=[\"-frtti\"]"
 else
-  gn gen out/Release --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false"
+  gn gen out/Release --args="is_official_build=true is_debug=false skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false"
 fi
 
 ninja -C out/Release skia
