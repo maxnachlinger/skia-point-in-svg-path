1c26d6ad179d7bce4597ffc000c03213441d01c8
Max Nachlinger <mnachlinger@walmartlabs.com>
2019-03-15 18:55:00 -0700

    Skia build work

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# docker rm $(docker ps -a -q)
-# docker rmi $(docker images -q)
+# if you want to clear al the docker things :)
+#docker rm $(docker ps -a -q) --force
+#docker rmi $(docker images -q) --force
 
 docker build --tag=ubuntudev -f ./docker/Ubuntu .
 
 id=$(docker create ubuntudev)
-rm -rf include
-docker cp $id:/include ./include
+rm -rf ./include
+mkdir ./include/skia/out/Release
+docker cp $id:/.skia-build/skia/out/Release ./include/skia/out/Release
 
 docker rm -v $id
