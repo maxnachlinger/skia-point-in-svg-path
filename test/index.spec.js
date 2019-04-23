ea57eec34a23fb0bdfd22ad6a2bcb2ec36dc55fb
Max Nachlinger <maxnachlinger@users.noreply.github.com>
2019-04-23 15:49:06 -0700

    Update index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -13,7 +13,7 @@ describe('getPathsContainingPoints tests', () => {
     expect(getPathsContainingPoints(
       [{id: 'path-0', data: 'test'}],
       [{id: 'point-0', x: 100, y: 100}],
-    )).toEqual([{intersectingPathIds: [], pointId: 'point-0'}]);
+    )).toEqual([{intersectingPathIds: [], pointId: 'point-0', x: 100, y: 100}]);
   });
 
   it('correctly identifies a point inside a path', () => {
@@ -22,31 +22,31 @@ describe('getPathsContainingPoints tests', () => {
         [{id: 'path-0', data: 'M80 80A 45 45, 0, 0, 0, 125 125L 125 80 Z'}],
         [{id: 'point-0', x: 100, y: 100}],
       ],
-      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0', x: 100, y: 100}],
     }, {
       input: [
         [{id: 'path-0', data: 'M80 80A 45 45, 0, 0, 0, 125 125L 125 80 Z'}],
         [{id: 'point-0', x: 100.1, y: 100.1}],
       ],
-      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0', x: 100.1, y: 100.1}],
     }, {
       input: [
         [{id: 'path-0', data: 'M230 80A 45 45, 0, 1, 0, 275 125L 275 80 Z'}],
         [{id: 'point-0', x: 220, y: 100}],
       ],
-      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0', x: 220, y: 100}],
     }, {
       input: [
         [{id: 'path-0', data: 'M80 230A 45 45, 0, 0, 1, 125 275L 125 230 Z'}],
         [{id: 'point-0', x: 115, y: 235}],
       ],
-      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0', x: 115, y: 235}],
     }, {
       input: [
         [{id: 'path-0', data: 'M230 230A 45 45, 0, 1, 1, 275 275L 275 230 Z'}],
         [{id: 'point-0', x: 300, y: 235}],
       ],
-      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: ['path-0'], pointId: 'point-0', x: 300, y: 235}],
     }].forEach(({input, expected}) => {
       expect(getPathsContainingPoints(...input)).toEqual(expected);
     });
@@ -58,25 +58,25 @@ describe('getPathsContainingPoints tests', () => {
         [{id: 'path-0', data: 'M80 80A 45 45, 0, 0, 0, 125 125L 125 80 Z'}],
         [{id: 'point-0', x: 200, y: 100}],
       ],
-      expected: [{intersectingPathIds: [], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: [], pointId: 'point-0', x: 200, y: 100}],
     }, {
       input: [
         [{id: 'path-0', data: 'M230 80A 45 45, 0, 1, 0, 275 125L 275 80 Z'}],
         [{id: 'point-0', x: 220, y: 50}],
       ],
-      expected: [{intersectingPathIds: [], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: [], pointId: 'point-0', x: 220, y: 50}],
     }, {
       input: [
         [{id: 'path-0', data: 'M80 230A 45 45, 0, 0, 1, 125 275L 125 230 Z'}],
         [{id: 'point-0', x: 115, y: 200}],
       ],
-      expected: [{intersectingPathIds: [], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: [], pointId: 'point-0', x: 115, y: 200}],
     }, {
       input: [
         [{id: 'path-0', data: 'M230 230A 45 45, 0, 1, 1, 275 275L 275 230 Z'}],
         [{id: 'point-0', x: 250, y: 235}],
       ],
-      expected: [{intersectingPathIds: [], pointId: 'point-0'}],
+      expected: [{intersectingPathIds: [], pointId: 'point-0', x: 250, y: 235}],
     }].forEach(({input, expected}) => {
       expect(getPathsContainingPoints(...input)).toEqual(expected);
     });
