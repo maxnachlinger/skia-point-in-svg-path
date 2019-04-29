342fa667728579c3d9ec12801f3d6a440ee5f310
Max Nachlinger <max.nachlinger@gmail.com>
2019-04-29 15:36:36 -0700

    Perf tests

--- example/index.js
+++ example/index.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const skiaPointInSvgPath = require('..');
+const pointInSvgPath = require('point-in-svg-path');
+
 const {points, paths} = require('./input');
 
-const label = `(${points.length}) points, (${paths.length}) paths`;
-console.time(label);
-const result = skiaPointInSvgPath(paths, points);
-console.timeEnd(label);
+// prepare input for point-in-svg-path
+const pathDataStrings = paths.map(({data}) => data);
+const pointInSvgPathInputs = points.reduce((acc, {x, y}) => acc.concat(
+  pathDataStrings.map((d) => [d, x, y])
+), []);
+
+const inputSize = `(${points.length}) points, (${paths.length}) paths`;
+
+console.time(`skia-point-in-svg-path: ${inputSize}`);
+skiaPointInSvgPath(paths, points);
+console.timeEnd(`skia-point-in-svg-path: ${inputSize}`);
+
 // console.log(JSON.stringify(result, null, 2))
